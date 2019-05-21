a4334dda2f3d5265288987253b2fdffa53c99fe5
William Sinn <w.h.sinn@gmail.com>
2019-05-21 19:15:53 -0400

    met all deliverables

--- README.md
+++ README.md
@@ -22,10 +22,10 @@ This will serve your code on http://localhost:3000.
 
 You will be using the following backend to get the list of books. The expectation here is that you will include the following features
 
-- Get a list of books & render them
+<!-- - Get a list of books & render them -->
   `http://localhost:3000/books`
-- Be able to click on a book, you should see the book's thumbnail and description and a list of users who have liked the book.
-- You can like a book by clicking on a button. You are user 1 `{"id":1, "username":"pouros"}`, so to like a book send a `PATCH` request to `http://localhost:3000/books/:id` with an array of users who like the book. This array should be equal to the existing array of users that like the book, plus your user. For example, if the previous array was `"[{"id":2, "username":"auer"}, {"id":8, "username":"goodwin"}]`, you should send as the body of your PATCH request:
+<!-- - Be able to click on a book, you should see the book's thumbnail and description and a list of users who have liked the book. -->
+<!-- - You can like a book by clicking on a button. You are user 1 `{"id":1, "username":"pouros"}`, so to like a book send a `PATCH` request to `http://localhost:3000/books/:id` with an array of users who like the book. This array should be equal to the existing array of users that like the book, plus your user. For example, if the previous array was `"[{"id":2, "username":"auer"}, {"id":8, "username":"goodwin"}]`, you should send as the body of your PATCH request: -->
 
 ```javascript
 {
@@ -36,5 +36,5 @@ You will be using the following backend to get the list of books. The expectatio
   ]
 }
 ```
-- This route will respond with the updated book json including the list of users who have liked the book.
+<!-- - This route will respond with the updated book json including the list of users who have liked the book. -->
 - BONUS: Can you make it so a second patch request to the same book removes your user from the list of users? Can you toggle likes on and off?
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   const listUL = document.querySelector("#list")
   const showPanel = document.querySelector("#show-panel")
   const listPanel = document.querySelector("#list-panel")
+  const user1 = {"id":1, "username":"pouros"};
+
 
   fetch("http://localhost:3000/books", {method: "GET"}).then(resp => resp.json())
   .then(function(booksObject){
@@ -19,25 +21,47 @@ document.addEventListener("DOMContentLoaded", function() {
       showPanel.innerHTML=``
       fetch(`http://localhost:3000/books/${bookId}`).then(resp => resp.json())
       .then(function(book){
-        let followers = book.users.length
         showPanel.innerHTML+=`<div id="b${book.id}">
                               <h1>${book.title}</h1>
-                              <img src="${book.img_url}">
+                              <img src="${book.img_url}"><button id="${book.id}"> follow & like </button>
                               <p>${book.description}</p>
                               </div>
-                              <div style="display:block;"><span style="background-color:green; height:20px; width:20px; justify-content:center; color:red; padding-left:4px; border-radius:6px; border:1px solid red; margin-right:4px;">  ${followers}  </span><span><b>  Readers Following<b></span></div>
+                              <div style="display:block;"><span style="background-color:green; height:20px; width:20px; justify-content:center; color:black; padding-left:4px; border-radius:6px; border:1px solid red; margin-right:4px;">  ${book.users.length}  </span><span><b>  Readers Following</b></span></div>
                               `
         for (let user in book.users) {
         let readers = book.users[user]
           showPanel.innerHTML+=`
-          <span id="u${readers.id}">
+                                <span id="u${readers.id}">
                                 <h4>${readers.username}</h4>
                                 </span>
                                `
         }
       })
   })//render book info on clicking title
-
+  showPanel.addEventListener("click", event => {
+      let targetBook = event.target
+      let getBookId = parseInt(targetBook.id)
+      let followString = targetBook.parentElement.parentElement.children[1]
+      let followNum = parseInt(followString.innerText)
+      followString.innerText = `${followNum + 1}`
+      fetch(`http://localhost:3000/books/${getBookId}`, {
+                                    method:"PATCH",
+                                    headers: {
+                                    "Content-Type":"application/json",
+                                    Accept: "application/json"
+                                  },body: JSON.stringify({"users": [{"id":1, "username":"pouros"}]})
+                                }).then(resp => resp.json())
+                                .then(book => {
+                                  for (let user in book.users) {
+                                  let readers = book.users[user]
+                                  showPanel.innerHTML+=`
+                                    <span id="u${readers.id}">
+                                    <h4>${readers.username}</h4>
+                                    </span>
+                                  `
+                                }
+                              })
+  })
 
 
 
